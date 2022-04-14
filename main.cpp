0c56d0ae13c8252b0365a07ce5d1d0ff9bb6976e
Elena Antipina <lena.ypa@yandex.ru>
2022-04-14 20:50:51 +0500

    +f

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
+    int a, b, c, f;
       cout << "Enter two numbers";
       cin >> a >> b;
+      c = a + b;
       cout << "C = a+b = " << c;
+      f = a - b;
+      cout << "F = a-b = " << f;
       return 0;
 }
